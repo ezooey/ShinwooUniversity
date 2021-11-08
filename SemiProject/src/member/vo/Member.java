5daad336299944eaa37424e54b7d0c8f9fa75c81
eunhye3333 <eunhye_3@naver.com>
2021-11-08 22:23:03 +0900

    21.11.08 기본 파일 세팅

---
+++ SemiProject/src/member/vo/Member.java
@@ -0,0 +1,109 @@
+package member.vo;
+
+public class Member {
+	private String menberId;
+	private String memberPwd;
+	private String memberName;
+	private String department;
+	private String phone;
+	private String address;
+	private String email;
+	private String memberType;
+	private String status;
+	
+	public Member() {}
+	
+	public Member(String menberId, String memberPwd, String memberName, String department, String phone, String address,
+			String email, String memberType, String status) {
+		super();
+		this.menberId = menberId;
+		this.memberPwd = memberPwd;
+		this.memberName = memberName;
+		this.department = department;
+		this.phone = phone;
+		this.address = address;
+		this.email = email;
+		this.memberType = memberType;
+		this.status = status;
+	}
+
+	public String getMenberId() {
+		return menberId;
+	}
+
+	public void setMenberId(String menberId) {
+		this.menberId = menberId;
+	}
+
+	public String getMemberPwd() {
+		return memberPwd;
+	}
+
+	public void setMemberPwd(String memberPwd) {
+		this.memberPwd = memberPwd;
+	}
+
+	public String getMemberName() {
+		return memberName;
+	}
+
+	public void setMemberName(String memberName) {
+		this.memberName = memberName;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getMemberType() {
+		return memberType;
+	}
+
+	public void setMemberType(String memberType) {
+		this.memberType = memberType;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	@Override
+	public String toString() {
+		return "Member [menberId=" + menberId + ", memberPwd=" + memberPwd + ", memberName=" + memberName
+				+ ", department=" + department + ", phone=" + phone + ", address=" + address + ", email=" + email
+				+ ", memberType=" + memberType + ", status=" + status + "]";
+	}
+	
+}
